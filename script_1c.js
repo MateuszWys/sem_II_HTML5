5ffca94d5fb0c4c048465bcf04be5477d6ff7e21
MateuszWys <72884715+MateuszWys@users.noreply.github.com>
2021-03-29 13:52:58 +0200

    Add files via upload

--- script_1c.js
+++ script_1c.js
@@ -54,3 +54,31 @@
     movies.forEach(movie => initializeLitenersForMovieItem(movie));
     renderList();
 })();
+
+function moveUp() {
+    var sel = document.getElementById("cols");
+    var i1=0, i2=1;
+    while (i2 < sel.options.length) {
+        swapIf(sel,i1++,i2++);
+    }
+}
+function moveDown() {
+    var sel = document.getElementById("cols");
+    var i1=sel.options.length-1, i2=i1-1;
+    while (i1 > 0) {
+        swapIf(sel,i1--,i2--);
+    }
+}
+var swapVar = '';
+function swapIf(sel,i1,i2) {
+    if ( ! sel[i1].selected && sel[i2].selected) {
+        swapVar = sel[i2].text;
+        sel[i2].text = sel[i1].text;
+        sel[i1].text = swapVar;
+        swapVar = sel[i2].value;
+        sel[i2].value = sel[i1].value;
+        sel[i1].value = swapVar;
+        sel[i1].selected = true;
+        sel[i2].selected = false;
+    }
+}
